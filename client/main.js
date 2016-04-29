2b9c37ca81e48c1796bef4935d8fd77008a597ad
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-04-29 23:31:15 +0300

    Render temperatures to a simple table

--- client/main.js
+++ client/main.js
@@ -1,4 +1,27 @@
 var primus = require('Primus').connect()
 var $ = require('jquery')
+var Bacon = require('baconjs')
 
-primus.on('data', data => $('#data').html(JSON.stringify(data)))
+const temperatures = Bacon.fromEvent(primus, 'data').filter(data => data.tag === 't')
+
+temperatures
+  .groupBy(temperature => temperature.instance)
+  .flatMap(streamByInstance => {
+    var rowCreator = streamByInstance.first().doAction(temperature => $('#temperatures').append(rowTemplate(temperature)))
+    return rowCreator.concat(streamByInstance)
+  })
+  .onValue(temperature => {
+    var $row = $(`tr.temperature${temperature.instance}`)
+    $row.find('td.temperature').html(temperature.temperature.toFixed(2) + '&deg;C')
+    $row.find('td.vcc').html((temperature.vcc / 1000).toFixed(3) + 'V')
+  })
+
+
+function rowTemplate(temperature) {
+  return `<tr class="temperature${temperature.instance}">
+            <td>Sensor ${temperature.instance}</td>
+            <td class="temperature"></td>
+            <td class="vcc"></td>
+          </tr>
+        `
+}
