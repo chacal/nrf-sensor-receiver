f06928c955074d18a2942aca93221adf41a139cb
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-05-02 22:31:09 +0300

    Render also pressures

--- client/main.js
+++ client/main.js
@@ -3,10 +3,14 @@ var $ = require('jquery')
 var Bacon = require('baconjs')
 var moment = require('moment')
 
-const temperatures = Bacon.fromEvent(primus, 'data').filter(data => data.tag === 't')
+const temperatures = eventsWithTag('t')
+const pressures = eventsWithTag('p')
 
 bindRenderer(temperatures, $('#temperatures'), temperatureRowTemplate, renderTemperature)
+bindRenderer(pressures, $('#pressures'), pressureRowTemplate, renderPressure)
 
+
+function eventsWithTag(tag) { return Bacon.fromEvent(primus, 'data').filter(data => data.tag === tag) }
 
 function bindRenderer(sensorStream, $displayTable, rowTemplateCreator, renderer) {
   sensorStream
@@ -25,10 +29,27 @@ function renderTemperature(temperature) {
   $row.find('td.time').html(moment().format('HH:mm:ss'))
 }
 
+function renderPressure(pressure) {
+  var $row = $(`tr.pressure${pressure.instance}`)
+  $row.find('td.pressure').html(pressure.pressure.toFixed(2) + 'mbar')
+  $row.find('td.vcc').html((pressure.vcc / 1000).toFixed(3) + 'V')
+  $row.find('td.time').html(moment().format('HH:mm:ss'))
+}
+
 function temperatureRowTemplate(temperature) {
   return `<tr class="temperature${temperature.instance}">
             <td>Sensor ${temperature.instance}</td>
             <td class="temperature"></td>
+            <td class="vcc"></td>
+            <td class="time"></td>
+          </tr>
+        `
+}
+
+function pressureRowTemplate(pressure) {
+  return `<tr class="pressure${pressure.instance}">
+            <td>Sensor ${pressure.instance}</td>
+            <td class="pressure"></td>
             <td class="vcc"></td>
             <td class="time"></td>
           </tr>
