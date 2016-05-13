f809cbcd3ac6a7ceee9836fe68cccb4a7132f06b
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-05-13 10:39:10 +0300

    Add bootstrap support for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,13 @@ module.exports = {
         query: {
           presets: ['es2015']
         }
-      }
+      },
+      {test: /\.css$/, loaders: ['style', 'css']},
+      {test: /\.less$/, loaders: ['style', 'css', 'less']},
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   },
   externals: [ 'Primus' ]
