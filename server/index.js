d5f1df7e6a7976154293b84ab26e971336f3aae6
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-05-19 08:24:02 +0300

    Proxy /signalk to SignalK server

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ var express = require('express')
 var http = require('http')
 var sensorStream = process.platform === 'linux' ? require('./nrf-receiver.js') : require('./sensor-simulator.js')
 var logToConsole = process.env.LOG_TO_CONSOLE
+var SIGNALK_SERVER = process.env.SIGNALK_SERVER_URL ? process.env.SIGNALK_SERVER_URL : 'http://10.90.100.1'  // Defaults to Freya
 var autopilot = process.platform === 'linux' ? require('./autopilot-controller.js') : require('./autopilot-simulator')
 var util = require('./util.js')
+var requestProxy = require('express-request-proxy')
 
 var app = express()
 app.use(express.static(__dirname + '/../public'))
-app.use(require('body-parser').json())
 var server = http.createServer(app)
 var primus = new Primus(server)
 
@@ -16,6 +17,10 @@ server.listen(8080, () => {
   console.log('Listening on http://localhost:8080')
   start()
 })
+
+app.use('/signalk/*', requestProxy({ url: `${SIGNALK_SERVER}/signalk/*` }))
+
+app.use(require('body-parser').json())
 
 app.post('/autopilot/state/:state', (req, res) => {
   if(req.params.state === 'on') {
