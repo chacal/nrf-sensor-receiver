35f3ec8e1722c9eac070705dd51661b52c224f31
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-06-19 22:22:05 +0300

    Add simulator source for tank level

--- server/sensor-simulator.js
+++ server/sensor-simulator.js
@@ -2,7 +2,7 @@ var Bacon = require('baconjs')
 var _ = require('lodash')
 
 var simulatorStream = Bacon.interval(2000)
-  .map(() => _.sample([randomTemperature, randomPressure, randomCurrent])())
+  .map(() => _.sample([randomTemperature, randomPressure, randomCurrent, randomTankLevel])())
 
 
 function randomTemperature() {
@@ -34,6 +34,17 @@ function randomCurrent() {
     rawMeasurement: _.random(-1000, 1000),
     shuntVoltageMilliVolts: _.random(-50, 50, true),
     current: _.random(-20, 40, true),
+    vcc: _.random(3500, 4200),
+    previousSampleTimeMicros: _.random(2000, 25000),
+    ts: new Date()
+  }
+}
+
+function randomTankLevel() {
+  return {
+    tag:  'w',
+    instance: _.random(1, 5),
+    tankLevel: _.random(0, 100),
     vcc: _.random(3500, 4200),
     previousSampleTimeMicros: _.random(2000, 25000),
     ts: new Date()
