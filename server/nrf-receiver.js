2b20bea19127d4c54dbca3a8da76abee7aa5a68f
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-06-28 00:14:28 +0300

    Use 250kbps as nRF data rate

--- server/nrf-receiver.js
+++ server/nrf-receiver.js
@@ -9,7 +9,7 @@ var config = {
   irqPin: 24,                    // GPIO_24
   channel: 76,                   // 76 is the default channel for RF24 Arduino library
   rxAddress: process.env.NRF24_RX_ADDRESS ? process.env.NRF24_RX_ADDRESS : "nrf01",
-  dataRate: '2Mbps',
+  dataRate: '250kbps',
   crcBytes: 2,
   txPower: 'PA_MAX'
 }
